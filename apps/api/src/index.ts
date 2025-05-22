b340f9d3b8ae92ace4f332e19e772e89dc4004e8
Shubhamxshah <ritikgujarat@gmail.com>
2025-05-22 21:27:09 +0530

    express added

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,12 @@
-import { SELL_ORDER } from "@repo/common/types"
+import express from "express";
+import cors from "cors"
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(() => {
+  console.log(`server listening on port ${PORT}`)
+})
