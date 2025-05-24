c7d34845e4ca1ddb280329ae126c7c3d4b35d3d9
Shubhamxshah <ritikgujarat@gmail.com>
2025-05-24 17:20:11 +0530

    tested phone auth

--- apps/frontend/src/app/dashboard/page.tsx
+++ apps/frontend/src/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { authClient } from '@/lib/auth-client';
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers";
 
+const Dashboard = async () => {
+  // const { data: session, error } = await authClient.getSession();
+  // console.log("session is" , session, error);
+  
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  console.log("server session", session?.user.id)
+  return <div>Dashboard
+    <pre>{JSON.stringify(session, null, 2)}</pre>
+  </div>;
+};
 
-export default Dashboard
+export default Dashboard;
