531899ca260a9717b541c48c9c7354c947faa0e8
Shubhamxshah <ritikgujarat@gmail.com>
2025-05-28 00:31:40 +0530

    added engine redis manager

---
+++ apps/engine/src/config/redisManager.ts
@@ -0,0 +1,30 @@
+import Redis from "ioredis";
+
+export class RedisManager {
+  private client: Redis;
+  private static instance: RedisManager;
+
+  constructor() {
+    this.client = new Redis(process.env.REDIS_URL || 'redis://localhost:6379')
+    this.client.connect();
+  }
+
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new RedisManager;
+    }
+    return this.instance;
+  }
+
+  public sendToApi(message: string, clientId: string) {
+      this.client.publish(clientId, JSON.stringify(message));
+  }
+
+  public pushToArchiver(message: ArchiverMessage) {
+    this.client.lpush("messageToArchiver", JSON.stringify(message))
+  }
+
+  public publishMessage(channel: string, message: WsMessage) {
+    this.client.publish(channel, JSON.stringify(message))
+  }
+}
