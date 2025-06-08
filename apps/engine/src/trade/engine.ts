45644e9a90c984aa23178359552984f896c0b24d
Shubhamxshah <ritikgujarat@gmail.com>
2025-06-08 14:26:20 +0530

    balance

--- apps/engine/src/trade/engine.ts
+++ apps/engine/src/trade/engine.ts
@@ -241,7 +241,7 @@ export class Engine {
     try {
       this.orderBook.set('btc', eventInitialize);
       this.balances.set('e0XMZgd8t5zR3B6t6os1euTMrXoMeRk3', {
-        available: 50000000,
+        available: 80000000,
         locked: 0,
       });
 
