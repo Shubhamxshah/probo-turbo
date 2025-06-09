388e8da9177974035ca0a341b3cd597d198972be
Shubhamxshah <ritikgujarat@gmail.com>
2025-06-10 03:52:38 +0530

    added route for landingheader

--- apps/frontend/src/components/LandingHeader.tsx
+++ apps/frontend/src/components/LandingHeader.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import { Button } from "@repo/ui/components/base/button"
 import { Poppins } from 'next/font/google'
+import { useRouter } from "next/navigation"
+
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -8,6 +10,7 @@ const poppins = Poppins({
 })
 
 const Landingheader = () => {
+  const router = useRouter();
   return (
     <div className="w-full block md:flex max-w-7xl mx-auto justify-between">
     <div className=" pt-40">
@@ -16,7 +19,7 @@ const Landingheader = () => {
       <div className="text-slate-600 pt-6 px-1 font-mono text-xl" > cricket, entertainment, trade and finance</div>
       <div className="pt-6 ">
         <Button variant="outline" className="mr-2" > download app</Button>
-        <Button> trade online </Button>  
+        <Button onClick={() => router.push("/auth")}> trade online </Button>  
       </div>
     </div>
     <div >
